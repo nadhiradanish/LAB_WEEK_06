0e1c93751b2bd6ebb6c5ad4f71529cf78de1a287
nadhiradanish <152785547+nadhiradanish@users.noreply.github.com>
2025-09-28 21:59:33 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.lab_week_06
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.ImageLoader
+
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader
+) : RecyclerView.Adapter<CatViewHolder>() {
+
+    // Mutable list for storing all the list data
+    private val cats = mutableListOf<CatModel>()
+
+    // A function to set the mutable list
+    fun setData(newCats: List<CatModel>) {
+        cats.clear()
+        cats.addAll(newCats)
+        // This is used to tell the adapter that there's a data change in the mutable list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+        val view = layoutInflater.inflate(R.layout.item_list, parent, false)
+        return CatViewHolder(view, imageLoader)
+    }
+
+    override fun getItemCount() = cats.size
+
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
+        holder.bindData(cats[position])
+    }
+}
---
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.lab_week_06
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.CatAdapter
+import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.model.CatBreed
+import com.example.lab_week_06.model.Gender
+import com.example.lab_week_06.GlideImageLoader // pastikan GlideImageLoader ada di sini
+
+class MainActivity : AppCompatActivity() {
+
+    private val recyclerView: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.recycler_view)
+    }
+
+    private val catAdapter by lazy {
+        CatAdapter(layoutInflater, GlideImageLoader(this))
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Setup the adapter for the recycler view
+        recyclerView.adapter = catAdapter
+
+        // Setup the layout manager for the recycler view
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+        // Add data to the model list in the adapter
+        catAdapter.setData(
+            listOf(
+                CatModel(
+                    Gender.Male,
+                    CatBreed.BalineseJavanese,
+                    "Fred",
+                    "Silent and deadly",
+                    "https://cdn2.thecatapi.com/images/7dj.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.ExoticShorthair,
+                    "Wilma",
+                    "Cuddly assassin",
+                    "https://cdn2.thecatapi.com/images/egv.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.AmericanCurl,
+                    "Curious George",
+                    "Award winning investigator",
+                    "https://cdn2.thecatapi.com/images/bar.jpg"
+                )
+            )
+        )
+    }
+}
